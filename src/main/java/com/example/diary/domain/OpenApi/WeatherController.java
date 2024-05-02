9d8dcad53ca8e4866936d492727974fdf7d2d34c
김멍몽 <wjdahrrla@gmail.com>
2024-05-02 20:13:06 +0900

    feat: WeatherController 구현

---
+++ src/main/java/com/example/diary/domain/OpenApi/WeatherController.java
@@ -0,0 +1,57 @@
+package com.example.diary.domain.OpenApi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@RestController
+public class WeatherController {
+
+    private String key = "84b3aa8d66b95d7753695e0cf431e713";
+
+//    @GetMapping("/weather/{lon}/{lat}")
+    @GetMapping("/weather/{city}")
+//    public String getWeather(@PathVariable String lat, @PathVariable String lon)
+    public String getWeather(@PathVariable String city){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            // String APIUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key;
+
+            String APIUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&appid=" + key;
+
+            URL url = new URL(APIUrl);
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("Content-type", "application/json");
+
+            BufferedReader bufferedReader;
+
+            if(urlConnection.getResponseCode() >= 200 && urlConnection.getResponseCode() <= 300) {
+                bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            }
+            else {
+                bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getErrorStream()));
+            }
+
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+
+            bufferedReader.close();
+            urlConnection.disconnect();
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+}
